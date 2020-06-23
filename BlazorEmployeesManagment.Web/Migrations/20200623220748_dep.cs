1719cca9f0cac38ab651d45a0bfa86f350b522b0
sheriff-kurdi <sheriff.kurdi@gmail.com>
2020-06-24 00:27:46 +0200

    Add project files.

---
+++ BlazorEmployeesManagment.Web/Migrations/20200623220748_dep.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorEmployeesManagment.Web.Migrations
+{
+    public partial class dep : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees");
+        }
+    }
+}
---
+++ BlazorEmployeesManagment.Web/Services/EmployeesServices.cs
@@ -0,0 +1,56 @@
+﻿using BlazorEmployeesManagment.Web.Data;
+using BlazorEmployeesManagment.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployeesManagment.Web.Services
+{
+    public class EmployeesServices
+    {
+        private readonly ApplicationDbContext db;
+
+        public EmployeesServices(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async void Add (Employee employee)
+        {
+            await db.Employees.AddAsync(employee);
+            await db.SaveChangesAsync();
+        }
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            return await db.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetById(int id)
+        {
+            return await db.Employees
+                .Include(e => e.Department)
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+        public async void Delete(int id)
+        {
+            var employee = await GetById(id);
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+        }
+
+        public async void Update(int id, Employee employee)
+        {
+            var oldEmployee = await GetById(id);
+            oldEmployee.FirstName = employee.FirstName;
+            oldEmployee.LastName = employee.LastName;
+            oldEmployee.Email = employee.Email;
+            oldEmployee.DateOfBrith = employee.DateOfBrith;
+            oldEmployee.Gender = employee.Gender;
+            oldEmployee.DepartmentId = employee.DepartmentId;
+            oldEmployee.PhotoPath = employee.PhotoPath;
+
+        }
+    }
+}
